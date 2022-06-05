bdd9da533979ec43b87afd00da755bde2b405ee8
Jeseok09 <blackn133@gmail.com>
2022-06-05 09:52:18 +0900

    Updates

---
+++ precache-manifest.6f0cda153ffb010f9c495de8a7bfe9ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab465015c80f54363c00",
+    "url": "/movie_app_20170190/static/css/main.f7ac9427.chunk.css"
+  },
+  {
+    "revision": "ab465015c80f54363c00",
+    "url": "/movie_app_20170190/static/js/main.ab465015.chunk.js"
+  },
+  {
+    "revision": "a2a6862979f91c33a84e",
+    "url": "/movie_app_20170190/static/js/1.a2a68629.chunk.js"
+  },
+  {
+    "revision": "3d3f7113338793c4c53a",
+    "url": "/movie_app_20170190/static/js/runtime~main.3d3f7113.js"
+  },
+  {
+    "revision": "b6c8e4c2931063654e9d3fbb7154dcc0",
+    "url": "/movie_app_20170190/index.html"
+  }
+];
